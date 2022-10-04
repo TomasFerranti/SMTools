151e48d018514cf158cc53b57f832e227db05268
tomas <tomas_ferranti_07@hotmail.com>
2022-10-04 03:28:05 -0300

    partition scripts

--- improve_edge.py
+++ improve_edge.py
@@ -6,11 +6,15 @@ from scipy.ndimage import gaussian_filter
 from numba import jit
 
 # transformar uma imagem cinza com valores entre 0 e 1 para um formato reconhecido pelo imshow
+
+
 def toRGB(image):
-    image = np.round(255*image)
-    return np.concatenate(3*[image.reshape((image.shape[0],image.shape[1],1))], axis = 2).astype(np.uint8)
+    image = np.round(255 * image)
+    return np.concatenate(3 * [image.reshape((image.shape[0], image.shape[1], 1))], axis=2).astype(np.uint8)
 
 # iterar sobre uma lista de pontos p0 e p1 para achar o melhor par em uma matriz binaria de arestas
+
+
 @jit(nopython=True)
 def optimizePoints(p0_list, p1_list, edges):
     best_score = 0
@@ -19,7 +23,7 @@ def optimizePoints(p0_list, p1_list, edges):
     for cur_p0 in p0_list:
         for cur_p1 in p1_list:
             x0, y0, x1, y1 = cur_p0[0], cur_p0[1], cur_p1[0], cur_p1[1]
-            
+
             # SOURCE BEGIN
             # https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
             dx = abs(x1 - x0)
@@ -33,23 +37,26 @@ def optimizePoints(p0_list, p1_list, edges):
             while True:
                 xcoordinates.append(x0)
                 ycoordinates.append(y0)
-                if x0 == x1 and y0 == y1: break
+                if x0 == x1 and y0 == y1:
+                    break
                 e2 = 2 * error
                 if e2 >= dy:
-                    if x0 == x1: break
+                    if x0 == x1:
+                        break
                     error = error + dy
                     x0 = x0 + sx
                 if e2 <= dx:
-                    if y0 == y1: break
+                    if y0 == y1:
+                        break
                     error = error + dx
                     y0 = y0 + sy
             # SOURCE END
-                
+
             score = 0
             for i in range(len(xcoordinates)):
-                score += edges[ycoordinates[i],xcoordinates[i]]
+                score += edges[ycoordinates[i], xcoordinates[i]]
             score = score / len(xcoordinates)
-            #print("score:",score,"xcoords:",xcoordinates,", ycoords:", ycoordinates)
+
             if score > best_score:
                 best_score = score
                 best_p0 = cur_p0
@@ -57,23 +64,40 @@ def optimizePoints(p0_list, p1_list, edges):
     return best_p0, best_p1, best_score
 
 # plotar a aresta melhorada junto com sua versao anterior
-def segsPlot(p0, p1, best_p0, best_p1, edges, image, fig_size = (20,10)):
+
+
+def segsPlot(p0, p1, best_p0, best_p1, edges, image, fig_size=(20, 10)):
     fig, axs = plt.subplots(2, 1, figsize=fig_size, dpi=80)
-    min_x = min(int(np.min([best_p0[0], best_p1[0]])-10), int(np.min([p0[0], p1[0]])-10))
-    min_y = min(int(np.min([best_p0[1], best_p1[1]])-10), int(np.min([p0[1], p1[1]])-10))
-    max_x = max(int(np.max([best_p0[0], best_p1[0]])+10), int(np.max([p0[0], p1[0]])+10))
-    max_y = max(int(np.max([best_p0[1], best_p1[1]])+10), int(np.max([p0[1], p1[1]])+10))
+    min_x = min(int(np.min([best_p0[0], best_p1[0]]) - 10),
+                int(np.min([p0[0], p1[0]]) - 10))
+    min_y = min(int(np.min([best_p0[1], best_p1[1]]) - 10),
+                int(np.min([p0[1], p1[1]]) - 10))
+    max_x = max(int(np.max([best_p0[0], best_p1[0]]) + 10),
+                int(np.max([p0[0], p1[0]]) + 10))
+    max_y = max(int(np.max([best_p0[1], best_p1[1]]) + 10),
+                int(np.max([p0[1], p1[1]]) + 10))
+
+    previous_edge = {'x': [p0[0] - min_x, p1[0] - min_x],
+                     'y': [p0[1] - min_y, p1[1] - min_y]}
+    improved_edge = {'x': [best_p0[0] - min_x, best_p1[0] - min_x],
+                     'y': [best_p0[1] - min_y, best_p1[1] - min_y]}
+
     axs[0].imshow(image[min_y:max_y, min_x:max_x, :])
-    axs[0].plot([p0[0]-min_x, p1[0]-min_x], [p0[1]-min_y, p1[1]-min_y], c = 'r')
-    axs[0].plot([best_p0[0]-min_x, best_p1[0]-min_x], [best_p0[1]-min_y, best_p1[1]-min_y], c = 'y')
+    axs[0].plot(previous_edge['x'], previous_edge['y'], c='r')
+    axs[0].plot(improved_edge['x'], improved_edge['y'], c='y')
+
     axs[1].imshow(toRGB(edges[min_y:max_y, min_x:max_x]))
-    axs[1].plot([p0[0]-min_x, p1[0]-min_x], [p0[1]-min_y, p1[1]-min_y], c = 'r')
-    axs[1].plot([best_p0[0]-min_x, best_p1[0]-min_x], [best_p0[1]-min_y, best_p1[1]-min_y], c = 'g')
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+    axs[1].plot(previous_edge['x'], previous_edge['y'], c='r')
+    axs[1].plot(improved_edge['x'], improved_edge['y'], c='g')
+
+    plt.subplots_adjust(left=None, bottom=None, right=None,
+                        top=None, wspace=None, hspace=None)
     plt.tight_layout()
     plt.show()
 
 # Criar pixels de bresenham entre dois pontos de coordenadas inteiras
+
+
 def bresenham(p0, p1):
     # SOURCE BEGIN
     # https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
@@ -94,14 +118,17 @@ def bresenham(p0, p1):
     while True:
         xcoordinates.append(x0)
         ycoordinates.append(y0)
-        if x0 == x1 and y0 == y1: break
+        if x0 == x1 and y0 == y1:
+            break
         e2 = 2 * error
         if e2 >= dy:
-            if x0 == x1: break
+            if x0 == x1:
+                break
             error = error + dy
             x0 = x0 + sx
         if e2 <= dx:
-            if y0 == y1: break
+            if y0 == y1:
+                break
             error = error + dx
             y0 = y0 + sy
     # SOURCE END
@@ -111,36 +138,46 @@ def bresenham(p0, p1):
     return p_list
 
 # criar lista de pares de pontos ortogonais ao segmento dado um raio
+
+
 def createPointsOrt(p0, p1, radius):
     sub = (p0-p1)
-    unit_vec = sub/np.linalg.norm(sub)
+    unit_vec = sub / np.linalg.norm(sub)
     unit_vec = np.array([-unit_vec[1], unit_vec[0]])
 
     p0_up = np.round(p0 + radius * unit_vec)
     p0_down = np.round(p0 - radius * unit_vec)
     p0_list = bresenham(p0_up, p0_down)
-    
+
     p1_up = np.round(p1 + radius * unit_vec)
     p1_down = np.round(p1 - radius * unit_vec)
     p1_list = bresenham(p1_up, p1_down)
 
     return p0_list.astype(np.int32), p1_list.astype(np.int32)
 
-# função principal que realiza o melhoramento das arestas
-def improveEdges(img, edges, plot = False):
-    # lendo as dimensões da imagem
-    height = img.shape[0]
-    width = img.shape[1]
 
+def cannyGaussian(img):
     # usando o detector de arestas Canny
     mid = cv.Canny(img, 30, 150)
 
     # aplicar filtro gaussiano 5x5
-    edgesPre = (mid/255)
+    edgesPre = (mid / 255)
     s = 1
     w = 5
-    t = (((w - 1)/2)-0.5)/s
+    t = (((w - 1) / 2) - 0.5) / s
     edgesMatrix = gaussian_filter(edgesPre, sigma=s, truncate=t)
+    return edgesMatrix
+
+# função principal que realiza o melhoramento das arestas
+
+
+def improveEdges(img, edges, plot=False, edgesMatrix=None):
+    # lendo as dimensões da imagem
+    height = img.shape[0]
+    width = img.shape[1]
+
+    if type(edgesMatrix) == type(None):
+        edgesMatrix = cannyGaussian(img)
 
     improvedEdges = []
     for p0, p1 in edges:
@@ -150,9 +187,10 @@ def improveEdges(img, edges, plot = False):
         best_score = 0
         best_p0 = p0
         best_p1 = p1
-        rOrt = np.ceil(min(width,height)/100)
+        rOrt = np.ceil(min(width, height) / 100)
         p0_listOrt, p1_listOrt = createPointsOrt(p0, p1, rOrt)
-        best_p0, best_p1, best_score = optimizePoints(p0_listOrt, p1_listOrt, edgesMatrix)
+        best_p0, best_p1, best_score = optimizePoints(
+            p0_listOrt, p1_listOrt, edgesMatrix)
         improvedEdges.append([list(best_p0), list(best_p1)])
 
         if plot:
@@ -165,6 +203,6 @@ def improveEdges(img, edges, plot = False):
             print("best p1:", best_p1)
             print("best score:", best_score)
             print("---------------------------")
-            segsPlot(p0, p1, best_p0, best_p1, edgesMatrix, img, (20,5))
+            segsPlot(p0, p1, best_p0, best_p1, edgesMatrix, img, (20, 5))
 
     return improvedEdges
--- improve_edges_json.py
+++ improve_edges_json.py
@@ -1,65 +1,73 @@
 import improve_edge
-import json
 import sys
-import cv2 as cv
-import numpy as np
 import matplotlib.pyplot as plt
+import copy
 
-# Carregar json
-filePath = "..\\Produto1\\TextureExtractor\\public\\calib\\" + sys.argv[1] + " - Copia" + ".json"
-with open(filePath, 'r') as file:
-    data = json.load(file)
+from shared_functions import saveToFile, readJson, readImage
 
-# Carregar e corrigir offset na imagem e arestas
-imgPath = "..\\Produto1\\TextureExtractor\\public\\images\\" + data['nomeImagem'] + "." + data['extensao']
-img = cv.imread(imgPath)
-iWidth, iHeight = img.shape[1], img.shape[0]
-cWidth, cHeight = 1200, 800
-wInicio = 0
-hInicio = 0 
-cEscala = 0
-aspectCanvas = cWidth/cHeight
-aspectImg = iWidth/iHeight
-if(aspectCanvas > aspectImg):
-    cEscala = cHeight/iHeight
-    wInicio = int(np.trunc((cWidth - cEscala*iWidth)/2))
-else:
-    cEscala = cWidth/iWidth
-    hInicio = int(np.trunc((cHeight - cEscala*iHeight)/2))
-img = cv.resize(img, (int(cEscala*iWidth), int(cEscala*iHeight)))
 
-initialEdges = []
-improvedEdges = []
-# Melhorar as arestas guias
-for i in range(0,3):
-    data['pontosguia'][i] = [[x-wInicio, y-hInicio] for x,y in data['pontosguia'][i]]
-    edges = [[data['pontosguia'][i][2*j], data['pontosguia'][i][2*j+1]] for j in range(int(len(data['pontosguia'][i])/2))]
-    best_edges = improve_edge.improveEdges(img, edges, plot=False)
-    data['pontosguia'][i] = [edge for edge_pair in best_edges for edge in edge_pair]
-    data['pontosguia'][i] = [[int(x+wInicio), int(y+hInicio)] for x,y in data['pontosguia'][i]]
-    initialEdges.append(edges)
-    improvedEdges.append(best_edges)
+def improveEdges(img_dict, data):
+    data_improved = copy.deepcopy(data)
+    edgesMatrix = improve_edge.cannyGaussian(img_dict['img'])
+    cEscala, wInicio, hInicio = img_dict['cEscala'], img_dict['wInicio'], img_dict['hInicio']
+
+    for i in range(0, 3):
+        data_improved['pontosguia'][i] = [[int((1 / cEscala) * (x - wInicio)),
+                                           int((1 / cEscala) * (y - hInicio))]
+                                          for x, y in data_improved['pontosguia'][i]]
+
+        edges = [[data_improved['pontosguia'][i][2*j],
+                  data_improved['pontosguia'][i][2*j + 1]]
+                 for j in range(int(len(data_improved['pontosguia'][i]) / 2))]
+
+        best_edges = improve_edge.improveEdges(
+            img_dict['img'], edges, plot=False, edgesMatrix=edgesMatrix)
+
+        data_improved['pontosguia'][i] = [
+            point for edge in best_edges for point in edge]
+        data_improved['pontosguia'][i] = [[int(cEscala * x + wInicio), int(cEscala * y + hInicio)]
+                                          for x, y in data_improved['pontosguia'][i]]
+    return data_improved
+
 
 def plotEdge(p0, p1, color, ax):
-    ax.plot([p0[0], p1[0]], [p0[1], p1[1]], c = color)
+    ax.plot([p0[0], p1[0]], [p0[1], p1[1]], c=color)
 
-plot = True
-if plot:
-    fig, axs = plt.subplots(1, 2, figsize=(10,20), dpi=80)
+
+def plotImprovement(img_dict, data, data_improved):
+    img = img_dict["img_canvas"].copy()
+    fig, axs = plt.subplots(1, 2, figsize=(10, 20), dpi=80)
     axs[0].imshow(img)
     axs[1].imshow(img)
     for i, c in enumerate(['r', 'g', 'b']):
-        for p0, p1 in initialEdges[i]:
+        edges = [[data['pontosguia'][i][2*j], data['pontosguia'][i][2*j + 1]]
+                 for j in range(len(data['pontosguia'][i]) // 2)]
+        edges_improved = [[data_improved['pontosguia'][i][2*j], data_improved['pontosguia'][i][2*j + 1]]
+                          for j in range(len(data_improved['pontosguia'][i]) // 2)]
+        for p0, p1 in edges:
             plotEdge(p0, p1, c, axs[0])
-        for p0, p1 in improvedEdges[i]:
+        for p0, p1 in edges_improved:
             plotEdge(p0, p1, c, axs[1])
     plt.show()
 
-# Salvar o resultado final
-filePath = "C:\\Users\\Tomás Ferranti\\Desktop\\IMS\\Produto1\\TextureExtractor\\public\\calib\\" + sys.argv[1] + ".json"
-save = True
-if save:
-    with open(filePath, 'w') as file:
-        file.seek(0)
-        json.dump(data, file, indent=4)
-        file.truncate()
+
+def improveJsonEdges(data):
+    img_dict = readImage(data)
+    data_improved = improveEdges(img_dict, data)
+    plot = True
+    if plot:
+        plotImprovement(img_dict, data, data_improved)
+
+    return data
+
+
+def main():
+    filename = sys.argv[1]
+    data = readJson(filename)
+    data = improveJsonEdges(data)
+    filepath_save = "cab_stereo_IMS/" + data['nomeImagem'] + ".json"
+    saveToFile(data, filepath_save)
+
+
+if __name__ == "__main__":
+    main()
